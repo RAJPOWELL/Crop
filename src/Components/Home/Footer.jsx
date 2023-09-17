5a5bad96a76f1ca9f4f4ea55453ac464f85de022
Raj Powell <raj@pop-os.localdomain>
2023-09-17 20:42:30 +0530

    Frontend Updated

---
+++ src/Components/Home/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Footer.scss';
+import { Link } from 'react-router-dom';
+
+// Import the TTF font
+import researcherFont from '/home/raj/Projects/Crop/src/Fonts/researcher/ttf/researcher.ttf';
+
+// Define a CSS class for the font
+const researcherTextStyle = {
+  fontFamily: 'Researcher, sans-serif', // Use the imported font
+  fontSize: '24px',
+  textAlign: 'center',
+   // Increase font weight to bold
+  // You can add other CSS styles like color, etc. here
+};
+
+function Footer() {
+  return (
+    <div className="section footerSection">
+      {/* Apply the custom font class to the <p> element */}
+      <p style={researcherTextStyle}>Team: <span style={{ fontWeight: 'bold' }}>Beta Qualifiers</span></p>
+
+      <div className="footerLinksDiv">
+        {/* Add your footer links here */}
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
+
---
+++ src/Components/Home/LandingSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LandingSection.scss'
+
+function LandingSection() {
+    return (
+        <div className="section landingSection">
+            <h1>Crop</h1>
+            <h2>Prediction & </h2>
+            <h2>Recommendation.</h2>
+            <p>Using Machine Learning Approaches</p>
+        </div>
+    )
+}
+
+export default LandingSection
